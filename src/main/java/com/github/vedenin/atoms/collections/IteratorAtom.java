442ac5aeaee84cfe1dda8c133b9c9cc4cc9ed27a
slava <vedenin1980@gmail.com>
2017-10-08 11:12:46 +0200

    Add collection atoms

---
+++ src/main/java/com/github/vedenin/atoms/collections/IteratorAtom.java
@@ -0,0 +1,9 @@
+package com.github.vedenin.thirdpartylib.collections;
+
+import java.util.Iterator;
+
+/**
+ * Created by vvedenin on 4/19/2017.
+ */
+public interface IteratorAtom<E> extends Iterator<E> {
+}
